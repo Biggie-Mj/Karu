5211a95ba07fd43e5487215276ea246eca987670
Biggie-Mj <ggt9v58cjr@privaterelay.appleid.com>
2026-09-17 12:17:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 // Change VERSION at every deployment; existing pages keep their current worker until closed.
-const VERSION='1.0.3';
+const VERSION='1.0.4';
 const PREFIX='karu-cache-'+encodeURIComponent(new URL(self.registration.scope).pathname)+'-';
 const CACHE=PREFIX+VERSION;
 const FILES=["./", "./index.html", "./styles.css", "./manifest.webmanifest", "./REGLES-ET-ARBITRAGES.md", "./app.js", "./feedback.js", "./gestures.js", "./data.js", "./source-texts.js", "./engine.js", "./storage.js", "./app-icon.png", "./01-accueil.webp", "./02-combat.webp", "./03-social.webp", "./04-journal.webp", "./baton.png", "./brassards.png", "./calebasse.png", "./carapace.png", "./ceinture.png", "./chaussons.png", "./deplacement.png", "./flechettes.png", "./griffes.png", "./inventaire.png", "./jets.png", "./journal.png", "./ki.png", "./main-ouverte.png", "./meditation.png", "./poing.png", "./reaction.png", "./repos.png", "./social.png", "./vitalite.png"];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith(PREFIX)&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
  const url=new URL(event.request.url);
